24780f7d89b078228b0c414dd9c3dc3805b4995a
Geno Yun <Geno Yun>
2017-04-13 22:58:19 +0900

    Add uintellisense asset

---
+++ samples/Unity-Plugin/Assets/uIntelliSense/Editor/MenuItems.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+
+namespace LostPolygon.uIntelliSense {
+    public static class MenuItems {
+        [MenuItem("Tools/Lost Polygon/uIntelliSense")]
+        private static void ShowWindow() {
+            EditorWindow.GetWindow<XmlDocsGeneratorWindow>(true, "uIntelliSense", true);
+        }
+
+        // This is required because for some reason Unity
+        // occasionally disables the menu item (for some people)
+        [MenuItem("Tools/Lost Polygon/uIntelliSense", true)]
+        private static bool ShowWindowValidation() {
+            return true;
+        }
+    }
+}

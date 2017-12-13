8a1ce11884f735bebc647cc1b077d09cb517e2f6
Sangwon Choi <sangwon.choi@bhaptics.com>
2017-12-13 15:21:04 +0900

    Edit unity plugin

---
+++ samples/tac-sharp-unity/Assets/bHapticsManager/Examples/Scripts/LongTest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Bhaptics.Tac.Unity
+{
+    public class LongTest : MonoBehaviour
+    {
+        private IHapticPlayer _player;
+
+        // Use this for initialization
+        void Start()
+        {
+            _player = BhapticsManager.HapticPlayer;
+            InvokeRepeating("TriggerBowShoot", .1f, 3f);
+            InvokeRepeating("TriggerElectricFront", 1f, 3f);
+        }
+
+        void TriggerBowShoot()
+        {
+            _player.SubmitRegistered("BowShoot");
+        }
+
+        void TriggerElectricFront()
+        {
+            _player.SubmitRegistered("ElectricFront");
+        }
+    }
+}

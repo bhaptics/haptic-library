346f4c44af2e5b5734d2c7d3ac0adc6de442dbd7
SanghunKim <sanghun.kim@bhaptics.com>
2021-01-11 20:28:03 +0900

    Change tactsuit image on android widget

--- samples/unity-plugin/Assets/Bhaptics/SDK/Scripts/BhapticsConfig.cs
+++ samples/unity-plugin/Assets/Bhaptics/SDK/Scripts/BhapticsConfig.cs
@@ -2,10 +2,10 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Data", menuName = "Bhaptics/Create Config", order = 1)]
-public class BhapticsConfig : ScriptableObject {
+public class BhapticsConfig : ScriptableObject
+{
     [Header("Windows Settings")]
     public bool launchPlayerIfNotRunning = true;
-
 
     [Header("Android Settings")]
     public BhapticsAndroidManager AndroidManagerPrefab;

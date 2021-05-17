ae972bc007e746c33d85cdbc937f8f0670045c89
jOoN <sam960313@gmail.com>
2021-05-17 17:16:32 +0900

    TactFileWatcher Inherits AssetPostprocessor instead of Editor

--- samples/unity-plugin/Assets/Bhaptics/SDK/Scripts/Editor/TactFileWatcher.cs
+++ samples/unity-plugin/Assets/Bhaptics/SDK/Scripts/Editor/TactFileWatcher.cs
@@ -1,126 +1,31 @@
-﻿using System.IO;
-using UnityEditor;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEditor;
+using System.IO;
 
 namespace Bhaptics.Tact.Unity
 {
-    public class TactFileWatcher : Editor
+    public class TactFileWatcher : AssetPostprocessor
     {
-        public static bool triggerRecompileForTactSource = false;
-        private static bool triggerRecompile = false;
-        private static bool triggerCheck = false;
-        private static FileSystemWatcher tactFileWatcher;
-        private static FileSystemWatcher tactClipWatcher;
-
-
-        [InitializeOnLoadMethod]
-        static void UpdateWatchers()
+        static void OnPostprocessAllAssets(
+             string[] importedAssets,
+             string[] deletedAssets,
+             string[] movedAssets,
+             string[] movedFromAssetPaths)
         {
-            BhapticsLogger.LogDebug("UpdateWatchers()");
-            RegisterWatcher(".tact");
-            RegisterTactClipDeleteWatcher();
-            EditorApplication.update += OnEditorApplicationUpdate;
-
-            triggerRecompile = CheckChanged();
-        }
-
-        private static void RegisterWatcher(string extenstion)
-        {
-#if UNITY_2017_1_OR_NEWER
-            tactFileWatcher = new FileSystemWatcher(Application.dataPath, "*" + extenstion);
-            tactFileWatcher.NotifyFilter = NotifyFilters.CreationTime |
-                NotifyFilters.Attributes |
-                NotifyFilters.DirectoryName |
-                NotifyFilters.FileName |
-                NotifyFilters.LastAccess |
-                NotifyFilters.LastWrite |
-                NotifyFilters.Security |
-                NotifyFilters.Size;
-
-            tactFileWatcher.IncludeSubdirectories = true;
-
-            //sWatcher.Changed += OnChanged;
-            tactFileWatcher.Created += OnCreated;
-            //sWatcher.Deleted += OnDeleted;
-            //sWatcher.Renamed += OnRenamed;
-
-            tactFileWatcher.EnableRaisingEvents = true;
-#else
-            BhapticsLogger.LogError("FileSystemWatcher required .Net 4.6 or higher");
-#endif
-        }
-
-        private static void RegisterTactClipDeleteWatcher()
-        {
-#if UNITY_2017_1_OR_NEWER
-            tactClipWatcher = new FileSystemWatcher(Application.dataPath, "*.asset");
-            tactClipWatcher.NotifyFilter = NotifyFilters.CreationTime |
-                NotifyFilters.Attributes |
-                NotifyFilters.DirectoryName |
-                NotifyFilters.FileName |
-                NotifyFilters.LastAccess |
-                NotifyFilters.LastWrite |
-                NotifyFilters.Security |
-                NotifyFilters.Size;
-
-            tactClipWatcher.IncludeSubdirectories = true;
-
-            tactClipWatcher.Deleted += OnTactClipDeleted;
-
-            tactClipWatcher.EnableRaisingEvents = true;
-#else
-            BhapticsLogger.LogError("FileSystemWatcher required .Net 4.6 or higher");
-#endif
-        }
-
-        private static void OnChanged(object source, FileSystemEventArgs e)
-        {
-            triggerRecompile = true;
-        }
-
-        private static void OnCreated(object source, FileSystemEventArgs e)
-        {
-            triggerRecompile = true;
-        }
-
-        private static void OnDeleted(object source, FileSystemEventArgs e)
-        {
-            triggerRecompile = true;
-        }
-
-        private static void OnRenamed(object source, RenamedEventArgs e)
-        {
-            triggerRecompile = true;
-        }
-
-        private static void OnTactClipDeleted(object source, FileSystemEventArgs e)
-        {
-            triggerCheck = true;
-        }
-
-
-
-        private static bool CheckChanged()
-        {
-            // TODO
-            return Directory.GetFiles("Assets/", "*.tact", SearchOption.AllDirectories).Length != 0;
-            // || BhapticsUtils.FindAssetsByType<TactClip>().Count != TactFileAsset.Instance.FeedbackFiles.Length;
-        }
-
-        private static void OnEditorApplicationUpdate()
-        {
-            if (triggerCheck)
+            foreach (string str in importedAssets)
             {
-                triggerRecompile = CheckChanged();
-                triggerCheck = false;
-            }
-            if (triggerRecompile)
-            {
+                BhapticsLogger.LogDebug("Import Assets");
                 HapticClipManager.RefreshTactFiles();
-                triggerRecompile = false;
-
-                triggerRecompileForTactSource = true;
+            }
+            foreach (string str in deletedAssets)
+            {
+                BhapticsLogger.LogDebug("Delete Assets");
+                if (Directory.GetFiles("Assets/", "*.tact", SearchOption.AllDirectories).Length != 0)
+                {
+                    HapticClipManager.RefreshTactFiles();
+                }
             }
         }
     }

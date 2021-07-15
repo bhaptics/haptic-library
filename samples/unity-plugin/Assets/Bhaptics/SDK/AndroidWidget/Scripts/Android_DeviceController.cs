2818b49f62ea7c2e0b436dbd515285bf4da93246
Sangwon Choi <sangwon.choi@bhaptics.com>
2021-07-15 22:11:49 +0900

    Update sample ui

---
+++ samples/unity-plugin/Assets/Bhaptics/SDK/AndroidWidget/Scripts/Android_DeviceController.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Bhaptics.Tact.Unity
+{
+    public class Android_DeviceController : MonoBehaviour
+    {
+        [Header("UI")]
+        [SerializeField] private Image icon;
+        [SerializeField] private Button pingButton;
+        [SerializeField] private Button lButton;
+        [SerializeField] private Button rButton;
+
+        [SerializeField] private Bhaptics_Widget_Setting widgetSetting;
+
+
+        private HapticDevice device;
+
+        void Awake()
+        {
+            if (pingButton != null)
+            {
+                pingButton.onClick.AddListener(Ping);
+            }
+            if (lButton != null)
+            {
+                lButton.onClick.AddListener(ToLeft);
+            }
+            if (rButton != null)
+            {
+                rButton.onClick.AddListener(ToRight);
+            }
+        }
+
+
+        public void RefreshDevice(HapticDevice d)
+        {
+            device = d;
+            if (device == null)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
+
+            if (!gameObject.activeSelf)
+            {
+                gameObject.SetActive(true);
+            }
+
+            UpdateIcon(d);
+        }
+
+        private void UpdateIcon(HapticDevice d)
+        {
+            switch (d.Position)
+            {
+                case PositionType.Vest:
+                    icon.sprite = GetSprite(widgetSetting.SettingTactot, d.IsConnected);
+                    break;
+                case PositionType.FootL:
+                case PositionType.FootR:
+                    icon.sprite = GetSprite(widgetSetting.SettingTactosyFoot, d.IsConnected);
+                    break;
+                case PositionType.HandL:
+                case PositionType.HandR:
+                    icon.sprite = GetSprite(widgetSetting.SettingTactosyHand, d.IsConnected);
+                    break;
+                case PositionType.ForearmL:
+                case PositionType.ForearmR:
+                    icon.sprite = GetSprite(widgetSetting.SettingTactosyArm, d.IsConnected);
+                    break;
+                case PositionType.Head:
+                    icon.sprite = GetSprite(widgetSetting.SettingTactal, d.IsConnected);
+                    break;
+
+                default:
+                    icon.sprite = null;
+                    break;
+            }
+        }
+
+        private Sprite GetSprite(SettingDeviceIcon icon, bool connected)
+        {
+            return connected ? icon.pairImage : icon.unpairImage;
+        }
+
+        private void Ping()
+        {
+            if (device == null)
+            {
+                return;
+            }
+
+            BhapticsAndroidManager.Ping(device);
+        }
+
+
+        private void ToLeft()
+        {
+            if (device == null)
+            {
+                return;
+            }
+
+            if (IsRight(device.Position))
+            {
+                BhapticsAndroidManager.TogglePosition(device.Address);
+            }
+        }
+        private void ToRight()
+        {
+            if (device == null)
+            {
+                return;
+            }
+            if (IsLeft(device.Position))
+            {
+                BhapticsAndroidManager.TogglePosition(device.Address);
+            }
+        }
+
+        private static bool IsLeft(PositionType pos)
+        {
+            switch (pos)
+            {
+                case PositionType.FootL:
+                case PositionType.HandL:
+                case PositionType.ForearmL:
+                case PositionType.Left:
+                case PositionType.GloveLeft:
+                    return true;
+
+            }
+            return false;
+        }
+
+        private static bool IsRight(PositionType pos)
+        {
+            switch (pos)
+            {
+                case PositionType.FootR:
+                case PositionType.HandR:
+                case PositionType.ForearmR:
+                case PositionType.Right:
+                case PositionType.GloveRight:
+                    return true;
+
+            }
+            return false;
+        }
+    }
+
+}
---
+++ samples/unity-plugin/Assets/Bhaptics/SDK/AndroidWidget/Scripts/Android_UIController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Bhaptics.Tact.Unity
+{
+    public class Android_UIController : MonoBehaviour
+    {
+        [SerializeField] private Transform devicesContainer;
+        [SerializeField] private Android_DeviceController devicePrefab;
+
+        [Header("No Paired Device UI")]
+        [SerializeField] private GameObject noPairedDeviceUi;
+        [SerializeField] private Button helpButton;
+
+
+        [SerializeField] private GameObject helpUi;
+        [SerializeField] private Button helpCloseButton;
+
+        private List<Android_DeviceController> controllers = new List<Android_DeviceController>();
+        private int deviceListSize = 10;
+
+        void Awake()
+        {
+            for (int i = 0; i < deviceListSize; i++)
+            {
+                var go = Instantiate(devicePrefab, devicesContainer.transform);
+                go.gameObject.SetActive(false);
+                controllers.Add(go);
+            }
+
+            BhapticsAndroidManager.AddRefreshAction(Refresh);
+            if (helpButton != null)
+            {
+                helpButton.onClick.AddListener(OnHelp);
+            }
+
+            if (helpCloseButton != null)
+            {
+                helpCloseButton.onClick.AddListener(CloseHelpNotification);
+            }
+        }
+
+        private void Refresh()
+        {
+            var devices = BhapticsAndroidManager.GetDevices();
+
+            if (devices.Count == 0)
+            {
+                noPairedDeviceUi.SetActive(true);
+            }
+            else
+            {
+                noPairedDeviceUi.SetActive(false);
+                helpUi.gameObject.SetActive(false);
+            }
+
+            for (int i = 0; i < deviceListSize; i++)
+            {
+                if (i <= devices.Count - 1)
+                {
+                    controllers[i].RefreshDevice(devices[i]);
+                }
+                else
+                {
+                    controllers[i].gameObject.SetActive(false);
+                }
+            }
+        }
+
+        private void OnHelp()
+        {
+            if (helpUi != null)
+            {
+                helpUi.SetActive(true);
+            }
+        }
+
+        private void CloseHelpNotification()
+        {
+            if (helpUi != null)
+            {
+                helpUi.SetActive(false);
+            }
+        }
+    }
+}

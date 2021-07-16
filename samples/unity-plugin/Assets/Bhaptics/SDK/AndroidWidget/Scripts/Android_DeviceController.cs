6c76624d1aca9e96a8c38ba49174183b3f5271c4
Sangwon Choi <sangwon.choi@bhaptics.com>
2021-07-16 09:53:24 +0900

    Update ui

--- samples/unity-plugin/Assets/Bhaptics/SDK/AndroidWidget/Scripts/Android_DeviceController.cs
+++ samples/unity-plugin/Assets/Bhaptics/SDK/AndroidWidget/Scripts/Android_DeviceController.cs
@@ -1,19 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Bhaptics.Tact.Unity
 {
+    [Serializable]
+    public class PositonIconSetting
+    {
+        public Sprite connect;
+        public Sprite disconnect;
+    }
+
+    [Serializable]
+    public class IconSetting
+    {
+
+        [Header("[Setting Icons]")]
+        public PositonIconSetting Vest;
+        public PositonIconSetting Head;
+        public PositonIconSetting Arm;
+        public PositonIconSetting Foot;
+        public PositonIconSetting Hand;
+    }
+
     public class Android_DeviceController : MonoBehaviour
     {
         [Header("UI")]
         [SerializeField] private Image icon;
+
+        [SerializeField] private IconSetting widgetSetting;
+
+        [Header("Connect Menu")]
+        [SerializeField] private GameObject ConnectMenu;
         [SerializeField] private Button pingButton;
         [SerializeField] private Button lButton;
         [SerializeField] private Button rButton;
 
-        [SerializeField] private Bhaptics_Widget_Setting widgetSetting;
+        [Header("Disconnect Menu")] 
+        [SerializeField] private GameObject DisconnectMenu;
+
+        private static Color SelectColor = new Color(82f/255, 103f/255, 249f/255);
+        private static Color DisableColor = new Color(82f/255, 84f/255, 102f/255);
 
 
         private HapticDevice device;
@@ -50,6 +79,57 @@ namespace Bhaptics.Tact.Unity
             }
 
             UpdateIcon(d);
+
+            if (d.IsConnected)
+            {
+                RenderConnectMenu();
+            }
+            else
+            {
+                RenderDisconnectMenu();
+            }
+        }
+
+        private void RenderConnectMenu()
+        {
+            ConnectMenu.gameObject.SetActive(true);
+            DisconnectMenu.gameObject.SetActive(false);
+
+            UpdateButtons();
+        }
+
+        private void RenderDisconnectMenu()
+        {
+            ConnectMenu.gameObject.SetActive(false);
+            DisconnectMenu.gameObject.SetActive(true);
+        }
+
+        private void UpdateButtons()
+        {
+            if (IsLeft(device.Position) || IsRight(device.Position))
+            {
+                pingButton.gameObject.SetActive(false);
+                lButton.gameObject.SetActive(true);
+
+                rButton.gameObject.SetActive(true);
+                if (IsLeft(device.Position))
+                {
+                    lButton.image.color = SelectColor;
+                    rButton.image.color = DisableColor;
+                }
+                else
+                {
+                    lButton.image.color = DisableColor;
+                    rButton.image.color = SelectColor;
+                }
+            }
+            else
+            {
+                pingButton.gameObject.SetActive(true);
+                pingButton.image.color = SelectColor;
+                lButton.gameObject.SetActive(false);
+                rButton.gameObject.SetActive(false);
+            }
         }
 
         private void UpdateIcon(HapticDevice d)
@@ -57,22 +137,22 @@ namespace Bhaptics.Tact.Unity
             switch (d.Position)
             {
                 case PositionType.Vest:
-                    icon.sprite = GetSprite(widgetSetting.SettingTactot, d.IsConnected);
+                    icon.sprite = GetSprite(widgetSetting.Vest, d.IsConnected);
                     break;
                 case PositionType.FootL:
                 case PositionType.FootR:
-                    icon.sprite = GetSprite(widgetSetting.SettingTactosyFoot, d.IsConnected);
+                    icon.sprite = GetSprite(widgetSetting.Foot, d.IsConnected);
                     break;
                 case PositionType.HandL:
                 case PositionType.HandR:
-                    icon.sprite = GetSprite(widgetSetting.SettingTactosyHand, d.IsConnected);
+                    icon.sprite = GetSprite(widgetSetting.Hand, d.IsConnected);
                     break;
                 case PositionType.ForearmL:
                 case PositionType.ForearmR:
-                    icon.sprite = GetSprite(widgetSetting.SettingTactosyArm, d.IsConnected);
+                    icon.sprite = GetSprite(widgetSetting.Arm, d.IsConnected);
                     break;
                 case PositionType.Head:
-                    icon.sprite = GetSprite(widgetSetting.SettingTactal, d.IsConnected);
+                    icon.sprite = GetSprite(widgetSetting.Head, d.IsConnected);
                     break;
 
                 default:
@@ -81,9 +161,9 @@ namespace Bhaptics.Tact.Unity
             }
         }
 
-        private Sprite GetSprite(SettingDeviceIcon icon, bool connected)
+        private Sprite GetSprite(PositonIconSetting icon, bool connected)
         {
-            return connected ? icon.pairImage : icon.unpairImage;
+            return connected ? icon.connect : icon.disconnect;
         }
 
         private void Ping()
